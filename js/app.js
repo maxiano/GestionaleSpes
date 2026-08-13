a90812469d163d0eaa602002a4e33c646c88dba9
maxiano <maxiano@users.noreply.github.com>
2026-08-13 10:05:17 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+/**
+ * @file app.js
+ * @brief Gestionale Tecnico - Spes Montesacro
+ * @author Massimiliano Nanni
+ * @copyright © 2026 Spes Montesacro. Tutti i diritti riservati.
+ * 
+ * Questo software è riservato esclusivamente all'uso interno della società 
+ * sportiva Spes Montesacro. Ne è vietata la copia, la riproduzione o la 
+ * distribuzione non autorizzata.
+ */
 // LOGICA APPLICATIVA IN JAVASCRIPT MODULARE (FIREBASE V10 SDK) -->
 		import { AppCache } from './cache.js';
 		import { daysOfWeekIT, monthNamesIT, normalizeUserProfile } from './config.js';
