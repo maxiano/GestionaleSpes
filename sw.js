377e338097aa378422f57a6b343168d036694d58
maxiano <maxiano@users.noreply.github.com>
2026-08-13 10:08:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
+/**
+ * @file sw.js
+ * @brief Gestionale Tecnico - Spes Montesacro
+ * @author Massimiliano Nanni
+ * @copyright © 2026 Spes Montesacro. Tutti i diritti riservati.
+ * 
+ * Questo software è riservato esclusivamente all'uso interno della società 
+ * sportiva Spes Montesacro. Ne è vietata la copia, la riproduzione o la 
+ * distribuzione non autorizzata.
+ */
 self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
 });
