4baa922b1d3d83d37888e8e85d590019b2699dce
maxiano <maxiano@users.noreply.github.com>
2026-08-13 10:07:43 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,14 @@
+/**
+ * @file utils.js
+ * @brief Gestionale Tecnico - Spes Montesacro
+ * @author Massimiliano Nanni
+ * @copyright © 2026 Spes Montesacro. Tutti i diritti riservati.
+ * 
+ * Questo software è riservato esclusivamente all'uso interno della società 
+ * sportiva Spes Montesacro. Ne è vietata la copia, la riproduzione o la 
+ * distribuzione non autorizzata.
+ */
+
 export function formatDateIT(dateStr) {
     if (!dateStr) return '--/--/----';
     const parts = dateStr.split('-');
