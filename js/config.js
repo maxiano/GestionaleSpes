317f76e7e5a3b84ab7beae9d81223e5bfec54079
maxiano <maxiano@users.noreply.github.com>
2026-08-13 10:06:05 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,14 @@
+/**
+ * @file config.js
+ * @brief Gestionale Tecnico - Spes Montesacro
+ * @author Massimiliano Nanni
+ * @copyright © 2026 Spes Montesacro. Tutti i diritti riservati.
+ * 
+ * Questo software è riservato esclusivamente all'uso interno della società 
+ * sportiva Spes Montesacro. Ne è vietata la copia, la riproduzione o la 
+ * distribuzione non autorizzata.
+ */
+
 export const daysOfWeekIT = ['dom', 'lun', 'mar', 'mer', 'gio', 'ven', 'sab'];
 
 export const monthNamesIT = [
