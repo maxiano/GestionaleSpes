8acbbe8372e0c749533ddee665d3cec4adf1f5af
maxiano <maxiano@users.noreply.github.com>
2026-08-21 21:54:35 +0200

    Update AppStore.js

--- js/core/AppStore.js
+++ js/core/AppStore.js
@@ -1,12 +1,25 @@
 // core/AppStore.js
+import { normalizeUserProfile } from './config.js';
+
 class AppStore {
     constructor() {
         this.activeTeamId = null;
+        this.currentUser = null;
         this.currentUserRole = 'admin'; // 'admin' o 'coach'
         this.currentCoachTeamId = null;
         this.players = [];
         this.tournaments = [];
         this.callups = [];
+    }
+
+    setUser(rawUserData) {
+        this.currentUser = normalizeUserProfile(rawUserData);
+        this.currentUserRole = this.currentUser.role || 'coach';
+        
+        // Se l'utente ha delle squadre associate e non c'è una squadra attiva, imposta la prima disponibile
+        if (this.currentUser.teams && this.currentUser.teams.length > 0 && !this.activeTeamId) {
+            this.setTeam(this.currentUser.teams[0]);
+        }
     }
 
     setTeam(teamId) {
