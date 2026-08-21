464079e0ccb9f875f716312005e8bc0481d190b9
maxiano <maxiano@users.noreply.github.com>
2026-08-21 22:27:56 +0200

    Update cache.js

--- js/core/cache.js
+++ js/core/cache.js
@@ -1,13 +1,4 @@
-/**
- * @file cache.js
- * @brief Gestionale Tecnico - Spes Montesacro
- * @author Massimiliano Nanni
- * @copyright © 2026 Spes Montesacro. Tutti i diritti riservati.
- * 
- * Questo software è riservato esclusivamente all'uso interno della società 
- * sportiva Spes Montesacro. Ne è vietata la copia, la riproduzione o la 
- * distribuzione non autorizzata.
- */
+// core/cache.js
 
 export const AppCache = {
     players: {},
