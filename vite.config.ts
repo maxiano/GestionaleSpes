565dcdc0d1d9f590ae8f53ac1be78dbed8d3b81f
maxiano <maxiano@users.noreply.github.com>
2026-09-06 12:26:53 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,68 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    base: '/',
+    plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: [
+          'icon-192.png',
+          'icon-512.png',
+          'screenshot-wide.png',
+          'screenshot-mobile.png'
+        ],
+        manifest: {
+          name: 'Spes Montesacro Gestionale',
+          short_name: 'SpesApp',
+          start_url: './index.html',
+          display: 'standalone',
+          background_color: '#ffffff',
+          theme_color: '#01411C',
+          icons: [
+            {
+              src: './icon-192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: './icon-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any'
+            }
+          ],
+          screenshots: [
+            {
+              src: './screenshot-wide.png',
+              sizes: '1280x720',
+              type: 'image/png',
+              form_factor: 'wide',
+              label: 'Schermata Desktop del Gestionale'
+            },
+            {
+              src: './screenshot-mobile.png',
+              sizes: '750x1334',
+              type: 'image/png',
+              label: 'Schermata Mobile del Gestionale'
+            }
+          ]
+        },
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+        },
+        devOptions: {
+          enabled: true,
+          type: 'module',
+        },
+      }),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
