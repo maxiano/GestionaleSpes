6dd418f7758fd57f6391fb522848925f40bc73b1
maxiano <max.nanni@gmail.com>
2026-09-09 08:17:56 +0000

    fix(build): increase workbox maximumFileSizeToCacheInBytes for PWA precaching

--- vite.config.ts
+++ vite.config.ts
@@ -58,6 +58,7 @@ export default defineConfig(() => {
           ]
         },
         workbox: {
+          maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
           globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
           importScripts: ['./sw-push.js'],
         },
